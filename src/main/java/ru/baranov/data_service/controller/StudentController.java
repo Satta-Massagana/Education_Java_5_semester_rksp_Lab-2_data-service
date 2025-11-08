1f15e7c981c02433409e15d895b599af6e068d33
Satta-Massagana <satta444@gmail.com>
2025-11-08 16:34:57 +0500

    StudentController

--- src/main/java/ru/baranov/data_service/controller/StudentController.java
+++ src/main/java/ru/baranov/data_service/controller/StudentController.java
@@ -1,4 +1,37 @@
 package ru.baranov.data_service.controller;
 
-public class StudentController {
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.baranov.data.api.StudentDataApi;
+import ru.baranov.data.model.StudentDataCreateRequest;
+import ru.baranov.data.model.StudentDataResponse;
+import ru.baranov.data_service.data.Student;
+import ru.baranov.data_service.data.StudentRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController implements StudentDataApi {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public ResponseEntity<StudentDataResponse> createStudentDataInData(StudentDataCreateRequest request) {
+        Student student = new Student();
+        student.setName(request.getFullName());
+        student.setPassport(request.getPassport());
+        studentRepository.save(student);
+        StudentDataResponse response = new StudentDataResponse();
+        response.setId(student.getId());
+        response.setFullName(student.getName());
+        response.setPassport(student.getPassport());
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+  /*@Override
+  public ResponseEntity<StudentDataResponse> getStudentDataByIdFromData(Long id) {
+
+    return ResponseEntity.status(200).body(response);
+  }*/
 }
